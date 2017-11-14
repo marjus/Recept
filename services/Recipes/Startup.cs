2abb120d41da9a0e8c75ebd50fe2d07c629d2ecc
Marjus Nielsen <marjus@hotmail.com>
2017-11-14 22:17:26 +0100

    renamed service project, added db dependency injection

--- services/Recipes/Startup.cs
+++ services/Recipes/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace Recipes
 {
@@ -23,6 +24,8 @@ namespace Recipes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<Recipes.Context.RecipeContext>(opt => opt.UseSqlServer("DefaultConnection"));
             services.AddMvc();
         }
 
